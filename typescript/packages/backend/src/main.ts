890c57f153c21518a814bb375b45465bf590eaef
mikan3rd <mikan.no.kikoushi@gmail.com>
2021-12-31 16:26:15 +0900

    refactor: change backend directory

--- typescript/packages/backend/src/main.ts
+++ typescript/packages/backend/src/main.ts
@@ -1,9 +1,36 @@
 import { NestFactory } from "@nestjs/core";
+import { locale } from "dayjs";
+import { Request } from "express";
+import morgan, { token } from "morgan";
 
-import { AppModule } from "./app.module";
+import { AppModule } from "@/modules/app.module";
+
+import "dayjs/locale/ja";
+
+locale("ja");
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  const app = await NestFactory.create(AppModule, {
+    logger: ["debug", "warn", "error"],
+  });
+
+  token("graphql-query", (req: Request) => {
+    const { body } = req;
+
+    if (body === undefined) {
+      return;
+    }
+
+    const { operationName, variables } = body;
+    if (operationName !== undefined) {
+      return `\nOperation Name: ${operationName}\nVariables: ${JSON.stringify(variables)}`;
+    }
+    return;
+  });
+
+  app.use(morgan(":method :url :status :response-time ms :graphql-query"));
+  app.enableCors();
+  await app.listen(process.env.PORT ?? 3300);
 }
+
 bootstrap();
