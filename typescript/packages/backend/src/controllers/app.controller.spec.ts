94d9c34220a187ee78377f5d943018e222e3718f
mikan3rd <mikan.no.kikoushi@gmail.com>
2021-12-31 16:46:42 +0900

    chore: add scalar

--- typescript/packages/backend/src/controllers/app.controller.spec.ts
+++ typescript/packages/backend/src/controllers/app.controller.spec.ts
@@ -1,6 +1,6 @@
 import { Test, TestingModule } from "@nestjs/testing";
 
-import { AppService } from "../services/app.service";
+import { AppService } from "../interfaces/services/app.service";
 
 import { AppController } from "./app.controller";
 
--- typescript/packages/backend/src/modules/app.module.ts
+++ typescript/packages/backend/src/modules/app.module.ts
@@ -1,12 +1,24 @@
 import { Module } from "@nestjs/common";
+import { ConfigModule } from "@nestjs/config";
+import { GraphQLModule } from "@nestjs/graphql";
 
-import { AppService } from "../services/app.service";
-
-import { AppController } from "@/controllers/app.controller";
+import { BigIntScalar } from "@/interfaces/scalars/index.scalar";
+import { PrismaModule } from "@/modules/prisma.module";
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+    }),
+    GraphQLModule.forRoot({
+      autoSchemaFile: "schema.graphql",
+      buildSchemaOptions: {
+        dateScalarMode: "isoDate",
+      },
+    }),
+    PrismaModule,
+  ],
+  controllers: [],
+  providers: [BigIntScalar],
 })
 export class AppModule {}
